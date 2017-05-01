c6ef1caa97c039a95aa10c7542da358ddb599b44
Xichu Liu <xichu.liu@yahoo.com>
2017-05-01 14:48:52 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+from setuptools import setup
+
+setup(name='LLBHC',
+      version='0.1',
+      description='BHC by Li & Liu',
+      url='https://github.com/lishaoji/LLBHC',
+      author='Shaoji Li & Xichu Liu',
+      author_email='xichu.liu@yahoo.com',
+      license='None',
+      packages=['LLBHC'],
+      zip_safe=False)
+
+
+# In[1]:
+
+get_ipython().system('jupyter nbconvert --to script setup.ipynb')
+
+
+# In[ ]:
+
+
+
